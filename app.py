acd14b2437482b4f72525e918833837d2f7a2211
jingiru <skyjjw79@hanmail.net>
2025-02-08 03:59:35 +0900

    기본 형식 제작

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/practice-videos')
+def practice_videos():
+    return render_template('practice_videos.html')
+
+@app.route('/performance-videos')
+def performance_videos():
+    return render_template('performance_videos.html')
+
+@app.route('/board')
+def board():
+    return render_template('board.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("댄스 동아리 홈페이지가 로드되었습니다!");
+});
